ad953de09bb64ea8b58cbdd8864616e549011a60
Christian Sigg <christian@sigg-iten.ch>
2013-10-23 19:09:27 +0200

    rename vv to pp

--- inst/nscancor_examples.R
+++ inst/nscancor_examples.R
@@ -7,10 +7,10 @@ set.seed(1)
 ### Unconstrained CCA, produces identical results to calling 
 # cancor(nutrimouse$gene[ , 1:10], nutrimouse$lipid)
 
-ypredict <- function(X, yv, vv) {
+ypredict <- function(X, yv, pp) {
   return(ginv(X)%*%yv)
 }
-xpredict <- function(Y, xv, vv) {
+xpredict <- function(Y, xv, pp) {
   return(ginv(Y)%*%xv)
 } 
 nscancor(nutrimouse$gene[ , 1:10], nutrimouse$lipid, xpredict=xpredict, 
@@ -19,12 +19,12 @@ nscancor(nutrimouse$gene[ , 1:10], nutrimouse$lipid, xpredict=xpredict,
 
 ### Non-negative sparse CCA using glmnet() as the regression function
 
-ypredict <- function(X, yv, vv) {
+ypredict <- function(X, yv, pp) {
     en <- glmnet(X, yv, alpha=0.5, intercept=FALSE, dfmax=5, lower.limits=0)
     W <- coef(en)
     return(W[2:nrow(W), ncol(W)])
 }
-xpredict <- function(Y, xv, vv) {
+xpredict <- function(Y, xv, pp) {
     en <- glmnet(Y, xv, alpha=0.5, intercept=FALSE, dfmax=3, lower.limits=0)
     V <- coef(en)
     return(V[2:nrow(V), ncol(V)])
